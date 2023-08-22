705b161ab391a3f6ae7dfbc270071f185e6b47c9
Ogomo <ogomolinsen0792@gmail.com>
2023-08-22 10:09:00 -0700

    print char

---
+++ print_char.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * print_char - print a char
+  * @c: character
+  * Return: the len, 1
+  */
+int print_char(char c)
+{
+	int len = 0;
+
+	_putchar(c);
+	len++;
+	return (len);
+}
