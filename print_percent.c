bd464be64eb7b5f43c577b857a1eed7f293435ab
OscarM03 <oscarmutwiri03@gmail.com>
2023-08-22 21:13:42 +0300

    print_percent file

---
+++ print_percent.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * print_percent - prints percent
+  * @c: char
+  * Return: len
+  */
+int print_percent(char c)
+{
+	int len = 0;
+
+	_putchar(c);
+	len++;
+
+	return (len);
+}
