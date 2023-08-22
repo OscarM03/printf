f86799c8ecde41a744a673c660669b4b9d1fb848
Ogomo <ogomolinsen0792@gmail.com>
2023-08-22 11:05:30 -0700

    print_p.c

---
+++ print_p.c
@@ -0,0 +1,40 @@
+"main.h"
+/**
+  * _printf - duplicate of printf
+  * @format: format string
+  * @...: valiadic function
+  * Return: len
+  */
+int _printf(const char *format, ...)
+{
+	int i, len = 0;
+	va_list format_list;
+
+	if (format == NULL)
+	{
+		return (-1);
+	}
+	va_start(format_list, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == ' ' || format[i] == '\0')
+			{
+				return (-1);
+			}
+			else
+			{
+				len += helper_printf(format_list, format[i]);
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			len++;
+		}
+	}
+	va_end(format_list);
+	return (len);
+}
